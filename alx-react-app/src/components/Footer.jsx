a816395bdf7d5f262cc98f8d39ea47980398581d
Priscy <priscillaakwetey03@gmail.com>
2024-08-10 16:57:54 +0000

    adding my mistakes

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-function fottere(){
+function Footer(){
     return(
         <footer>
             <p>© 2023 City Lovers</p>
@@ -6,3 +6,4 @@ function fottere(){
     );
 }
 
+export default Footer;
--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -6,4 +6,4 @@ function MainContent(){
        
     );
 }
-
+export default MainContent;
