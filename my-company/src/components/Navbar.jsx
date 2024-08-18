c52e6a1238bf9890c8cbf04fb934f1a0a6933ee7
Priscy <priscillaakwetey03@gmail.com>
2024-08-18 14:57:16 +0000

    my company codes

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+function Navbar(){
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to= "/">Home</Link>
+                </li>
+                <li>
+                    <Link to= '/About'>About</Link>
+                </li>
+                <li>
+                    <Link to= '/Services'>Service</Link>
+                </li>
+                <li>
+                    <Link to='/Contact'>Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
