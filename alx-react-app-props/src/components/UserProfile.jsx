bbdce7bf4b39e89740629f4c458736becc4707ea
Priscy <priscillaakwetey03@gmail.com>
2024-08-18 13:32:25 +0000

    adding changes

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,12 @@
 import UserInfo from './UserInfo';
+import UserContext from './UserContext';
 
 function ProfilePage() {
   return (
         <div>
       <h1>Profile</h1>
       <UserInfo />
+      <UserContext/>
     </div>
   )
 }
