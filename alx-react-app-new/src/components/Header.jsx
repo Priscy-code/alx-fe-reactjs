25424b4f5aafaaf907b26f9bb245c568da666487
Priscy <priscillaakwetey03@gmail.com>
2024-08-18 12:21:09 +0000

    loving react

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <header stles ={{backgroundColor: 'navy', color: 'white', textAlign: "center"}}>
+            <h1>My Favrite Cities</h1>
+        </header>
+    )
+}
+export default Header 
