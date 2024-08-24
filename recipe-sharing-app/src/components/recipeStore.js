24f20e313e4b9305ae7412c26d0d3d2f01e9974a
Priscy <priscillaakwetey03@gmail.com>
2024-08-24 17:20:36 +0000

    added update

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,9 +2,15 @@ import create from 'zustand'
 
 const useRecipeStore = create(set => ({
     recipes:[],
+    updateRecipe: (newRecipe) => set(state => ({
+      recipes: [...state.receipes, newRecipe]
+    })),
     addRecipe:(newRecipe) => set(state => ({
         receipes: [...state.recipes, newRecipe]
     })), 
+    deleteRecipe:id => set(state => ({
+      recipes: state.recipes.filter(recipe => recipe.id !== id)
+    })),
     setRecipes: (recipes) => set ({recipes})
 }));
 
