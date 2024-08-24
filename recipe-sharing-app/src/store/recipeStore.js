b8fb36bdeddfdc424a0c2dbdfaf0763b2cf3181b
Priscy <priscillaakwetey03@gmail.com>
2024-08-24 14:54:30 +0000

    Recipt project

---
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -0,0 +1,23 @@
+// import create from 'zustand'
+
+// const useRecipeStore = create(set => ({
+//     recipes:[],
+//     addRecipe:(newRecipe) => set(state => ({
+//         receipes: [...state.recipes, newRecipe]
+//     })), 
+//     setRecipes: (recipes) => set ({recipes})
+// }));
+
+// export default useRecipeStore
+
+import create from 'zustand';
+
+const useRecipeStore = create(set => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({
+    recipes: [...state.recipes, newRecipe]
+  })),
+  setRecipes: (recipes) => set({ recipes })
+}));
+
+export default useRecipeStore;
