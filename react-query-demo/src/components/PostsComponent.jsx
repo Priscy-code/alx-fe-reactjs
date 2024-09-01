72929eba701d688e91bef550b8f85a5c78be90ac
Priscy <priscillaakwetey03@gmail.com>
2024-09-01 13:40:40 +0000

    Rect dom

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,13 +1,24 @@
 import { useQuery } from "react-query";
 
 function PostComponent(){
-    const {isLoading, isError, fetchPosts} = useQuery(['posts'], () => 
+    const {isLoading, isError, fetchPosts, refetch} = useQuery(['posts'], () => 
     fetch('https://jsonplaceholder.typicode.com/posts'). then((response) => {
         if(!response.ok) {
             throw new Error('Network response was not okay')
         }
         return response.json();
-    }));
+    }),
+    {
+        cacheTime: 5 * 60 * 100,
+        staleTime: 30 * 1000,
+        refetchOnWindowFocus: true,
+        keepPreviousData:true,
+    }
+
+);
+
+
+
 
     if(isLoading) return <div>Loading</div>;
     if (isError) return <div>An error ocurred:{isError.message}</div>;
@@ -16,6 +27,7 @@ function PostComponent(){
     return(
         <div>
             <h2>Posts</h2>
+            <button onClick={() => refetch()}>Refetch Posts</button>
             <ul>
                 {data.map((post) =>(
                     <li key={post.id}>
