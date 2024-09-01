a04111c47d5b4c0f742ad262a1de551b9e5bcbad
Priscy <priscillaakwetey03@gmail.com>
2024-09-01 15:53:49 +0000

    Router

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter as Route, Routes} from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const Profile =() =>{
-    return(
+    // return(
+    //     <div>
+    //         <h1>Profile</h1>
+    //         <nav>
+    //             <ul>
+    //                 <li><Link to= "details">Profile Details</Link></li>
+    //                 <li><Link to="settings">Profile Settings</Link></li>
+    //             </ul>
+    //         </nav>
+    //     </div>
+    // )
+
+    return (
         <div>
-            <h1>Profile</h1>
-            <nav>
-                <ul>
-                    <li><Link to= "details">Profile Details</Link></li>
-                    <li><Link to="settings">Profile Settings</Link></li>
-                </ul>
-            </nav>
+            <Routes>
+                <Route path = "/" element = {<ProfileDetails/>}></Route>
+                <Route path = "setting " element = {<ProfileSettings/>}/>
+            </Routes>
         </div>
     )
 }
 
-export default Profile;
+export default Profile
