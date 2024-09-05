2bda730012120190fd57cffcdaa00f9791b1f163
Priscy <priscillaakwetey03@gmail.com>
2024-09-05 10:49:11 +0000

    tailwind project

--- tailwind-react-integration/src/assets/components/UserProfile.jsx
+++ tailwind-react-integration/src/assets/components/UserProfile.jsx
@@ -1,9 +1,13 @@
 function UserProfile(){
     return (
-        <div className="user-profile  bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-            <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto"/>
-            <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-            <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+        <div className="user-profile  bg-gray-100 md:p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 sm:max-w-xs">
+            <img src="https://via.placeholder.com/150" 
+            alt="User"
+             className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 "/>
+
+            <h1 className="md:text-xl text-blue-800 my-4 sm:text-lg">John Doe</h1>
+
+            <p className="sm:text-sm text-gray-600 md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
         </div>
     )
 }
