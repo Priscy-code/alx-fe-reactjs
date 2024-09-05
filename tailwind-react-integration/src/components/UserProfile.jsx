c14c594b6cc7ec5f713f13740abc6e0ab1a31142
Priscy <priscillaakwetey03@gmail.com>
2024-09-05 21:27:28 +0000

    tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile(){
         <div className="user-profile  bg-gray-100 md:p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 sm:max-w-xs hover:shadow-xl">
             <img src="https://via.placeholder.com/150" 
             alt="User"
-             className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 hover:scale-110 transition-transform duration-300 ease-in-out "/>
+             className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24 hover:scale-110 transition-transform duration-300 ease-in-out "/>
 
             <h1 className="md:text-xl text-blue-800 my-4 sm:text-lg hover:text-blue-500">John Doe</h1>
 
