8f26dd1247584966291da83cee2071f29a00eaa2
Priscy <priscillaakwetey03@gmail.com>
2024-09-11 13:10:04 +0000

    recipe platform

---
+++ recipe-sharing-platform/src/assets/components/HomePage.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import Recipe from '../../assets/receipe.jpg'
+
+const HomePage = () => {
+    const [recipes, setRecipes] = useState ([]);
+
+    useEffect(()=> {
+        const fetchData = async () => {
+            try{
+                const response = await fetch('data.json');
+                const data = await response.json();
+                setRecipes(data);
+
+            }catch(error){
+                console.error('Error fetching data: ', error)
+            }
+        };
+        fetchData();
+    }, []);
+
+    return(
+        <div className="container max-auto px-4 py-8 ">
+            <h1 className="text3xl font-bold text-center mb-8">Recipes</h1>
+            <div className="grid frid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {recipes.map((recipe) => (
+                    <div key={recipe.id} className="bg-white rounded-lg shadow-md p-4">
+                         <img src={Recipe} alt={recipe.title} className="w-full h-48 object-cover rounded-lg mb-4" />
+                         <h2 className="text-xl font-bold mb-2">{recipe.title}</h2>
+                          <p className="text-gray-700">{recipe.description}</p>
+               
+                    </div>
+                 ))}
+                   
+            </div>
+        </div>
+    )
+
+}
