c0f1d4d44b5f0108807ad71b04cb5d96a274626a
Priscy <priscillaakwetey03@gmail.com>
2024-09-14 19:15:16 +0000

    recipe done

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -21,11 +21,11 @@ const HomePage = () => {
     return(
         <div className="container max-auto px-4 py-8 ">
             <h1 className="text3xl font-bold text-center mb-8">Recipes</h1>
-            <div className="grid frid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-1 gap-4">
                 {recipes.map((recipe) => (
                     <div key={recipe.id} className="bg-white rounded-lg shadow-md p-4">
-                         <img src={Recipe} alt={recipe.title} className="w-full h-48 object-cover rounded-lg mb-4" />
-                         <h2 className="text-xl font-bold mb-2">{recipe.title}</h2>
+                         <img src={Recipe} alt={recipe.title} className="w-full h-48 object-cover rounded-lg mb-4 hover:scale-110" />
+                         <h2 className="text-xl font-bold mb-2 hover:text-blue-500">{recipe.title}</h2>
                           <p className="text-gray-700">{recipe.description}</p>
                
                     </div>
