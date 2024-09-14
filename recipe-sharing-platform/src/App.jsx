8f9d63bfac86e8cb5043f1bf5d8972944b27eec7
Priscy <priscillaakwetey03@gmail.com>
2024-09-14 19:19:48 +0000

    recipe

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import HomePage from './components/HomePage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import RecipeDetail from './components/RecipeDetail'
 
 const App = () => {
   return (
-    <div>
-      <HomePage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element ={<HomePage/>}/>
+        <Route path='/recipe/:id' element={<RecipeDetail/>}></Route>
+      </Routes>
+    </Router>
+    
   )
 }
 
