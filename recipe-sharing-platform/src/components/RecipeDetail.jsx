904cbbcc25941f759f7a5e5bffeb260a2c7d103e
Priscy <priscillaakwetey03@gmail.com>
2024-09-14 19:12:48 +0000

    section 2

---
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect} from "react"
+import { useParams } from "react-router-dom"
+
+const RecipeDetail = () => {
+    const { id } = useParams();
+    const [recipe, setRecipes] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchRecipe = async () => {
+            try{
+                const response = await fetch ('/data.json');
+                const data = await response.json();
+                const foundRecipe = data.recipes.find(recipe => recipe.id === parseInt(id))
+                setRecipes(foundRecipe);
+
+            }catch(err){
+                setError(err);
+            }finally{
+                setLoading(False);
+            }
+        };
+
+        fetchRecipe();
+    }, [id]);
+
+    if (loading) return <div className="text-center py-10"><p className="text-lg font-semibold"> Loading.... </p></div>
+    if(error) return <div className="text-center py-10"><p className="text-lg font-semibold text-red-500">Error loading recipe details.</p></div>
+    if (!recipe) return <div className="text-center py-10"><p className="text-lg font-semibold">Recipe not found.</p>
+</div>
+    return(
+    <div className="max-w-4xl mx-auto px-4 py-6">
+            <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+                <img src={recipes.image} alt={recipes.title} className="w-full h-64 object-cover"/>
+                <div className="p-6">
+                    <h1 className="text-3xl font-bold mb-4">{recipes.title}</h1>
+                    <p className="text-gray-700 text-lg">{recipes.summary}</p>
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default RecipeDetail;
+
+
+
