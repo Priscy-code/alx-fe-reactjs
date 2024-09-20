894da538332b7fec8c12da40224be2fb18655f87
Priscy <priscillaakwetey03@gmail.com>
2024-09-20 12:32:23 +0000

    api

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios, { Axios } from "axios"
 
-const fetchUserData = async({username, location , repository}) => {
+const fetchUserData = async({username, location , miniRepos}) => {
     let query ='';
     if (username ){
         query += `${username} in:login`
@@ -10,13 +10,14 @@ const fetchUserData = async({username, location , repository}) => {
         query += `location: ${location}`
     }
     if (repository) {
-        query += `respos:>= ${repository}`
+        query += `respos:>= ${miniRepos}`
     }
     if (!query){
         throw new ErrorEvent('At least one search criterion must be provided.')
     }
     try{
-        const url = `https://api.github.com/users/${username}`;
+        // const url = `https://api.github.com/users/${username}`;
+        const url = "https://api.github.com/search/users?q"
         const response = await axios.get(`${SEARCH_API_URL}?q=${encodeURIComponent(query)}`);
         
         // const response = await axios.get(url);
